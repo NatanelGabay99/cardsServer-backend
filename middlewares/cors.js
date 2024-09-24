840d30c8ae3bd58b13cda940f9b2d8c8e50e2b4d
Natanel Gabay <natanelg1@gmail.com>
2024-09-24 15:58:12 +0300

    Fix CORS allowed origins

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -5,7 +5,9 @@ const corsMiddleware =  cors({
     "http://127.0.0.1:5500", 
     "www.tzachCards.co.il", 
     "http://localhost:5173", 
-    "http://localhost:8181"
+    "http://localhost:8181",
+    "http://localhost:5174",
+    "https://myclientcardsproject.onrender.com"
   ],
 });
 
